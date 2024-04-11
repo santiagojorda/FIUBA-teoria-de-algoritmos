bff063294351bb867bf5a19e5f940c324de68908
santiagojorda <sjorda@fi.uba.ar>
2024-04-11 17:18:19 -0300

    DyC4 - Analisis de complejidad

--- TP0/alumno.py
+++ TP0/alumno.py
@@ -1,10 +1,17 @@
-# La Escuela Nacional 32 "Alan Turing" de Bragado tiene una forma particular de requerir que los alumnos formen fila. En vez del clásico "de menor a mayor altura", lo hacen primero con alumnos yendo con altura decreciente, hasta llegado un punto que empieza a ir de forma creciente, hasta terminar con todos los alumnos.
+# La Escuela Nacional 32 "Alan Turing" de Bragado tiene una forma particular de requerir que los alumnos formen fila. 
+# En vez del clásico "de menor a mayor altura", lo hacen primero con alumnos yendo con altura decreciente, 
+# hasta llegado un punto que empieza a ir de forma creciente, hasta terminar con todos los alumnos.
 
 # Por ejemplo las alturas podrían ser 1.2, 1.15, 1.14, 1.12, 1.02, 0.98, 1.18, 1.23.
 
-#     Implementar una función indice_mas_bajo que dado un arreglo/lista de alumnos(*) que represente dicha fila, devuelva el índice del alumno más bajo, en tiempo logarítmico. Se puede asumir que hay al menos 3 alumnos. En el ejemplo, el alumno más bajo es aquel con altura 0.98.
+#     Implementar una función indice_mas_bajo que dado un arreglo/lista de alumnos(*) 
+#     que represente dicha fila, devuelva el índice del alumno más bajo, en tiempo logarítmico.
+#     Se puede asumir que hay al menos 3 alumnos. En el ejemplo, el alumno más bajo es aquel 
+#     con altura 0.98.
 
-#     Implementar una función validar_mas_bajo que dado un arreglo/lista de alumnos(*) y un índice, valide (devuelva True o False) si dicho índice corresponde al del alumno más bajo de la fila. (Aclaración: esto debería poder realizarse en tiempo constante)
+#     Implementar una función validar_mas_bajo que dado un arreglo/lista de alumnos(*) 
+#     y un índice, valide (devuelva True o False) si dicho índice corresponde al del alumno 
+#     más bajo de la fila. (Aclaración: esto debería poder realizarse en tiempo constante)
 
 # (*)
 # Los alumnos son de la forma:
@@ -16,7 +23,8 @@
 
 # Se puede acceder a la altura de un alumno haciendo varible_tipo_alumno.altura.
 
-# Importante: considerar que si la prueba de volumen no pasa, es probable que sea porque no están cumpliendo con la complejidad requerida. 
+# Importante: considerar que si la prueba de volumen no pasa, es probable que sea porque no 
+# están cumpliendo con la complejidad requerida. 
 
 def dividir_y_encontrar_indice_del_mas_chico(alumnos, inicio, final):
     if inicio == final:
